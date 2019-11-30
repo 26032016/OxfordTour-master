20fbc487f43ab88e08245bf0521e654e586efcab
Sam Phiri <sagunde@gmail.comgit config --listgit config --global user.email sagunde@gmail.com>
2019-11-30 14:23:40 +0000

    Initial commit

---
+++ app/src/main/java/com/example/a18049192/oxfordtour/Magdalen_College.java
@@ -0,0 +1,18 @@
+package com.example.a18049192.oxfordtour;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class Magdalen_College extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_magdalen__college);
+    }
+}
+
